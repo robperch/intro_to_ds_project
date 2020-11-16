3709ac5f9dc8245987a3c2149035268515f1248f
Roberto919 <robertosysm@gmail.com>
2020-11-16 12:39:12 -0600

    Craeting base files for this issue

---
+++ src/utils/data_dict.py
@@ -0,0 +1,96 @@
+## MODULE WITH INFORMATION ABOUT THE FEATURES THAT ARE INCLUDED IN THE DATASET
+
+
+
+
+
+"------------------------------------------------------------------------------"
+##############################
+## Data profiling functions ##
+##############################
+
+
+data_dict = {
+    "folio": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "fecha_creacion": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "hora_creacion": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "dia_semana": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "codigo_cierre": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "fecha_cierre": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "año_cierre": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "mes_cierre": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "hora_cierre": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "delegacion_inicio": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "incidente_c4": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "latitud": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "longitud": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "clas_con_f_alarma": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "tipo_entrada": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "delegacion_cierre": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "geopoint": {
+        "relevant": True,
+        "data_type": "-"
+    },
+    "mes": {
+        "relevant": True,
+        "data_type": "-"
+    }
+}
+
+
+
+
+
+"------------------------------------------------------------------------------"
+#################
+## END OF FILE ##
+#################
+"------------------------------------------------------------------------------"
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,1 +1,46 @@
-## MODULE WITH FUNCTIONS TO SAVE AND LOAD PICKLE
+## MODULE WITH FUNCTIONS TO SAVE AND LOAD PICKLE, AS WELL AS OTHER USEFULL FUNCTIONS.
+
+
+
+
+
+"------------------------------------------------------------------------------"
+#############
+## Imports ##
+#############
+
+import json
+
+
+
+
+
+"------------------------------------------------------------------------------"
+##############################
+## Data profiling functions ##
+##############################
+
+
+## Pretty print a dictionary and preserving special characters
+def json_dump_dict(dictionary):
+    """
+    Pretty print a dictionary and preserving special characters
+        args:
+            dictionary (dictionary): dict that will be pretty printed
+        returns:
+            -
+    """
+
+    print(json.dumps(dictionary, indent=4, ensure_ascii=False).encode("utf8").decode())
+
+    return
+
+
+
+
+
+"------------------------------------------------------------------------------"
+#################
+## END OF FILE ##
+#################
+"------------------------------------------------------------------------------"
