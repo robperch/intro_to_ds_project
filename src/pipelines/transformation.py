8ac42301d5980339121edd7031202dc52f006dd1
Roberto919 <robertosysm@gmail.com>
2020-11-20 09:21:14 -0600

    Creating functions templates

--- src/pipelines/transformation.py
+++ src/pipelines/transformation.py
@@ -1,1 +1,74 @@
 ## MODULE TO TRANSFORM DATA BASED ON DATA-TYPES AND SAVE RESULTS AS PICKLE
+
+
+
+
+
+"------------------------------------------------------------------------------"
+#############
+## Imports ##
+#############
+
+
+## Python libraries
+
+
+
+
+
+"------------------------------------------------------------------------------"
+#################################
+## Generic ancillary functions ##
+#################################
+
+
+## X
+def load_ingestion(path):
+    """
+    """
+
+    pass
+
+
+
+## X
+def save_transformation(col, df):
+    """
+    """
+
+    pass
+
+
+
+
+
+"------------------------------------------------------------------------------"
+##############################
+## Transformation functions ##
+##############################
+
+
+##
+def date_transformation(col, df):
+    """
+    """
+
+    pass
+
+
+
+##
+def numeric_tranformation(col, df):
+    """
+    """
+
+    pass
+
+
+
+##
+def categoric_trasformation(col, df):
+    """
+    """
+
+    pass
