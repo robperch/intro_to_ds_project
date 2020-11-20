f212aa9eb6338e410ead3c1ca93ee499403e3575
Roberto919 <robertosysm@gmail.com>
2020-11-20 09:30:34 -0600

    Adding documentation to template functions

--- src/pipelines/transformation.py
+++ src/pipelines/transformation.py
@@ -22,18 +22,29 @@
 #################################
 
 
-## X
+## Loading ingestion pickle as dataframe for transformation pipeline.
 def load_ingestion(path):
     """
+    Loading ingestion pickle as dataframe for transformation pipeline.
+        args:
+            path (string): location where the pickle that will be loaded is.
+        returns:
+            -
     """
 
     pass
 
 
 
-## X
-def save_transformation(col, df):
+## Save transformed data frame as pickle.
+def save_transformation(df, path):
     """
+    Save transformed data frame as pickle.
+        args:
+            df (dataframe): transformation resulting dataframe.
+            path (string): location where the pickle object will be stored.
+        returns:
+            -
     """
 
     pass
@@ -48,27 +59,45 @@ def save_transformation(col, df):
 ##############################
 
 
-##
+## Conduct relevant transformations to date variables.
 def date_transformation(col, df):
     """
+    Conduct relevant transformations to date variables.
+        args:
+            col (string): name of date column that will be transformed.
+            df (dataframe): df that will be transformed.
+        returns:
+            df (dataframe): resulting df with cleaned date column.
     """
 
     pass
 
 
 
-##
+## Conduct relevant transformations to numeric variables.
 def numeric_tranformation(col, df):
     """
+    Conduct relevant transformations to numeric variables.
+        args:
+            col (string): name of numeric column that will be transformed.
+            df (dataframe): df that will be transformed.
+        returns:
+            df (dataframe): resulting df with cleaned numeric column.
     """
 
     pass
 
 
 
-##
+## Conduct relevant transformations to categoric variables.
 def categoric_trasformation(col, df):
     """
+    Conduct relevant transformations to categoric variables.
+        args:
+            col (string): name of categoric column that will be transformed.
+            df (dataframe): df that will be transformed.
+        returns:
+            df (dataframe): resulting df with cleaned categoric column.
     """
 
     pass
