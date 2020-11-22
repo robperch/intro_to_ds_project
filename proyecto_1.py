4711b3c3f7d10469c52a81672303c8e43afe4df8
Roberto919 <robertosysm@gmail.com>
2020-11-22 15:28:15 -0600

    Creating basic content of main module

--- proyecto_1.py
+++ proyecto_1.py
@@ -1,1 +1,68 @@
-## MODULE TO EXECUTE ANCILLARY MODULES
+## MODULE TO EXECUTE PROJECT
+
+
+
+
+
+"------------------------------------------------------------------------------"
+#############
+## Imports ##
+#############
+
+
+## Python libraries
+
+
+## Ancillary modules
+
+
+
+
+
+"------------------------------------------------------------------------------"
+###########################
+## Main project function ##
+###########################
+
+
+## Master function that executes the project's modules.
+def main():
+    """
+    Master function that executes the project's modules.
+        args:
+            -
+        returns:
+            -
+    """
+
+    ## Executing ingestion function
+    ingest(path)
+
+    ## Executing transformation function
+    transform(path)
+
+    ## Executing feature engineering function
+    feature_enineering(path)
+
+
+
+
+
+"------------------------------------------------------------------------------"
+##########################################
+## Conditional to execute main function ##
+##########################################
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+"------------------------------------------------------------------------------"
+#################
+## End of file ##
+#################
+"------------------------------------------------------------------------------"
